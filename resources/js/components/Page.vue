f5b96f4e0153416d1c714c8bbc9be78f29989a6c
kiddy <mhudyaramadhana@gmail.com>
2020-02-16 20:52:41 +0700

    Base of Swiper, content, and fixed README.md

--- resources/js/components/Page.vue
+++ resources/js/components/Page.vue
@@ -5,12 +5,73 @@
       background: bgColor
     }"
     >
-        {{ index }}
+        <div class="container text-center px-5 py-2">
+            <div class="col-12 mb-5">
+                <lottie v-if="index === 1" :options="winkLottie" :height="180" :width="180"/>
+                <lottie v-else-if="index === 2" :options="monkeyLottie" :height="180" :width="180"/>
+                <lottie v-else-if="index === 3" :options="cheerLottie" :height="180" :width="180"/>
+                <lottie v-else-if="index === 4" :options="iceCreamLottie" :height="180" :width="180"/>
+
+                <h3 class="mt-1">{{ message }}</h3>
+                <div v-if="lastItem === index" class="btn-choice">
+                    <a href="#" v-on:click="greet($event, true)" class="btn btn-md btn-success" style="background: #74b9ff !important; border-color: #74b9ff !important">Let's Go <i class="fa fa-heart"></i></a>
+                    <a href="#" v-on:click="greet($event, false)" class="btn btn-md btn-danger" style="background: #c0392b !important; border-color: #c0392b !important">Meh</a>
+                </div>
+                <lottie v-if="isLike" :options="likeLottie" :height="165" :width="165"/>
+                <lottie v-if="isUnlike" :options="unlikeLottie" :height="165" :width="165"/>
+            </div>
+            <div class="col-12 mt-5">
+                <div class="fixed-bottom px-5">
+                    <h2 v-if="lastItem !== index"><i class="fa fa-angle-up"></i></h2>
+                    <h6 class="font-weight-light">Created by <a class="text-white" href="https://twitter.com/perogeremmer">Mr.Software Engineer (@perogeremmer)</a> using VueJS, Laravel, and Lottie ♥</h6>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Lottie from 'vue-lottie';
+    import wink from "../../images/json/wink"
+    import cheer from "../../images/json/cheer"
+    import iceCream from "../../images/json/icecream"
+    import monkey from "../../images/json/monkey"
+    import loading from "../../images/json/loading"
+    import like from "../../images/json/like"
+    import unlike from "../../images/json/unlike"
+
     export default {
+        components: {
+            'lottie': Lottie
+        },
+        data() {
+            return {
+                winkLottie: {
+                    animationData: wink,
+                },
+                iceCreamLottie: {
+                    animationData: iceCream,
+                },
+                monkeyLottie: {
+                    animationData: monkey,
+                },
+                cheerLottie: {
+                    animationData: cheer,
+                },
+                loadingLottie: {
+                    animationData: loading,
+                },
+                likeLottie: {
+                    animationData: like,
+                },
+                unlikeLottie: {
+                    animationData: unlike,
+                },
+                animationSpeed: 1,
+                isLike: false,
+                isUnlike: false
+            }
+        },
         props: {
             index: {
                 type: Number,
@@ -19,6 +80,37 @@
             bgColor: {
                 type: String,
                 default: ''
+            },
+            message: {
+                type: String,
+                default: ''
+            },
+            lottieImage: {
+                type: String,
+                default: ''
+            },
+            lastItem: {
+                type: Number,
+                default: false
+            },
+        },
+        methods: {
+            async greet(event, choice) {
+                event.preventDefault();
+                let _this = this;
+
+                $(".btn-choice").remove();
+                _this.getChoice(choice);
+            },
+            async getChoice(choice) {
+                if (choice) {
+                    this.isLike = true;
+                } else {
+                    this.isUnlike = true;
+                }
+            },
+            async timeout(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
             }
         }
     };
--- resources/js/components/SwiperHome.vue
+++ resources/js/components/SwiperHome.vue
@@ -8,20 +8,20 @@
         @beforeChange="beforeChange"
         @afterChange="afterChange"
     >
-        <swiper-item
-            v-for="(color, index) in colors"
-            :key="index"
-        >
-            <page
-                :index="index + 1"
-                :bg-color="color"
+        <swiper-item v-for="(item, index) in data" :key="index">
+            <page :index="parseInt(index) + 1"
+                  :last-item="parseInt(data.length)"
+                  :bg-color="item.color"
+                  :message="item.message"
+                  :lottie-image="item.lottie"
             />
         </swiper-item>
     </swiper>
 </template>
 
 <script>
-    import { Swiper, SwiperItem } from 'vue-h5-swiper';
+    import {Swiper, SwiperItem} from 'vue-h5-swiper';
+
     const PAGE_TRANSITIONS = [
         'glue',
         'move',
@@ -47,6 +47,29 @@
                     '#BB8FCE',
                     '#7FB3D5'
                 ],
+                data: [
+                    {
+                        "color": '#27ae60',
+                        "message": "Hi, apa kabar? Nampaknya kamu sedang tidak baik-baik saja.",
+                        "lottie": "",
+                    },
+                    {
+                        "color": '#d35400',
+                        "message": "Aku memang tidak bisa memberikan banyak hal untukmu, tapi aku harap ini dapat membuatmu menjadi lebih baik.",
+                        "lottie": "",
+                    },
+                    {
+                        "color": '#8e44ad',
+                        "message": "Bersedih tak apa, tapi jangan berlarut-larut ya. Tenang, semua akan baik-baik saja, mari kita perbaiki ini bersama.",
+                        "lottie": "",
+                    },
+                    {
+                        "color": '#2980b9',
+                        "message": "Bagaimana kalau kita makan es krim bersama sambil tertawa melupakan duka?",
+                        "lottie": "",
+                    },
+
+                ],
                 showIndicator: false,
                 pageTransition: PAGE_TRANSITIONS[0],
                 pageTransitions: PAGE_TRANSITIONS,
@@ -67,8 +90,10 @@
     body {
         margin: 0;
     }
+
     .app {
         position: relative;
+
         .operation {
             position: absolute;
             top: 16px;
